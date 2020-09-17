c8a502eeae6b50dfd77e246ed118e8718bb40b54
fredycorts7 <fred.cor.14@gmail.com>
2020-09-17 14:23:52 -0500

    <ADD> Implementando el componente inicial <NothingSelected />

--- src/components/journal/JournalScreen.jsx
+++ src/components/journal/JournalScreen.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { SideBar } from './SideBar';
+// import { NothingSelected } from './NothingSelected';
 
 export const JournalScreen = () => {
   return (
     <div className='journal__main-content'>
       <SideBar />
 
-      <main>
-        <h1>Main content</h1>
-      </main>
+      <main>{/* <NothingSelected /> */}</main>
     </div>
   );
 };
