1a669ea8dd60c93f92b82c6cd186a3bc4e43a790
fredycorts7 <fred.cor.14@gmail.com>
2020-09-18 15:26:51 -0500

    <ADD> Implementando el hook useSelector para obtener el state y poder mostrar el mensaje de error cuando sea necesario

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import validator from 'validator';
 import { removeError, setError } from '../../actions/ui';
@@ -7,6 +7,7 @@ import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
   const dispatch = useDispatch();
+  const { msgError } = useSelector((state) => state.ui);
 
   const [formValues, handleInputChange] = useForm({
     name: '',
@@ -49,7 +50,7 @@ export const RegisterScreen = () => {
       <h3 className='auth__title'>Register</h3>
 
       <form className='mt-5' onSubmit={handleRegister}>
-        <div className='auth__alert-error'>Algo ha salido mal</div>
+        {msgError && <div className='auth__alert-error'>{msgError}</div>}
 
         <input
           type='text'
