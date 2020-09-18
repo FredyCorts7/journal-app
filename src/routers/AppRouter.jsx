2711e6b3926af68988b30acd50a0782c17c7589b
fredycorts7 <fred.cor.14@gmail.com>
2020-09-18 18:52:49 -0500

    <ADD> Implementando un observable de firebase para conocer la ultima sesion establecida

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Route,
   BrowserRouter as Router,
   Switch,
   Redirect,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { firebase } from '../firebase/config';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    }); //onAuthStateChanged is a observable like useEffect
+  }, [dispatch]); //because run just one time
+
   return (
     <Router>
       <div>
