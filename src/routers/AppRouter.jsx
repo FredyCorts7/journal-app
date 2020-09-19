ab88ee0bd2e1726f905cade8bfbf1eb566f742b4
fredycorts7 <fred.cor.14@gmail.com>
2020-09-18 19:05:12 -0500

    <ADD> Implementando un state isAuthenticated para la proteccion de rutas

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Route,
   BrowserRouter as Router,
@@ -14,13 +14,24 @@ import { login } from '../actions/auth';
 export const AppRouter = () => {
   const dispatch = useDispatch();
 
+  const [checking, setChecking] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
       }
+      setChecking(false);
     }); //onAuthStateChanged is a observable like useEffect
-  }, [dispatch]); //because run just one time
+  }, [dispatch, setChecking]); //because run just one time
+
+  if (checking) {
+    return <h1>Wait ...</h1>;
+  }
 
   return (
     <Router>
