d9e4e2711f213188aa8be527fadda66522a2b492
fredycorts7 <fred.cor.14@gmail.com>
2020-09-19 23:41:27 -0500

    <ADD> Definiendo reducers de notes e implementando renderizacion condicional en <JournalScreen />

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,25 @@
+/* 
+{
+    notes: [],
+    active: null,
+    active: {
+        id: "io213oi2321o3p",
+        title: "",
+        body: "",
+        imageUrl: "",
+        date: 213213213
+    }
+}
+*/
+
+const initialState = {
+  notes: [],
+  active: null,
+};
+
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { authReducer } from '../reducers/authReducer';
+import { notesReducer } from '../reducers/notesReducer';
 import { uiReducer } from '../reducers/uiReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -8,6 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducers = combineReducers({
   auth: authReducer,
   ui: uiReducer,
+  notes: notesReducer,
 });
 
 export const store = createStore(
